0971267ca929776fceb3113aeb76745fc3d7a05a
Kektus8000 <simy8000@gmail.com>
2024-08-03 15:28:24 +0200

    Aggiunta funzione per la creazione di un indirizzo

--- entities/Customer/customer.cpp
+++ entities/Customer/customer.cpp
@@ -1,6 +1,5 @@
 #include "customer.h"
-#include <string>
-#include <cassert>
+
 
 // Costruttore di Customer
 Customer::Customer(
@@ -25,4 +24,37 @@ Customer::Customer(
     Mail = mail;
 
     Abita = città;   
+}
+
+void AggiungiIndirizzo(
+    Con2DB db1,
+    std::string via,
+    int civico,
+    std::string cap,
+    std::string city,
+    std::string stato
+){
+    // Res immagazzina le risposte del database ed eventuali errori
+    PGResult *res
+    
+    // Effettuati controlli sui parametri per fare in modo che rispettino i limiti richiesti
+    assert(via.length() > 0 && via.length() <= 100);
+    assert(civico >= 0);
+    assert(cap.length() == 5);
+    assert(city.length() > 0 && city.length() <= 30);
+    assert(stato.length() > 0 && stato.length() <= 30);
+
+    /* sprintf si occupa di creare una stringa, dandogli un format e dei parametri
+    sprintf(destinazione, formattazione, parametri)
+    ESEMPIO:
+    sprintf(stringa, "%d + %d fa %d, facile!", 10, 12, 22)
+    stringa sarà uguale a "10 + 12 fa 22, facile!"
+    */
+    sprintf(comando,
+    "INSERT INTO Indirizzo (via, civico, cap, citta, stato)
+        VALUES (\'%s\', \'%d\', \'%s\', \'%s\', \'%s\') 
+        ON CONFLICT DO NOTHING",
+	    via, civico, cap, city, stato);
+    res = db1.ExecSQLcmd(comando);
+    PQclear(res);
 }
--- entities/Customer/customer.h
+++ entities/Customer/customer.h
@@ -1,6 +1,17 @@
 #define customer_h
 
 #include <string>
+#include "../../lib/con2db/pgsql.h"
+#include <cassert>
+// Su VSC mi da errore con l'include (probabilmente perchè uso Windows)
+
+struct Indirizzo{
+    std::string via;
+    int civico;
+    std::string cap;
+    std::string city;
+    std::string stato;
+};
 
 class Customer{
     public:
@@ -15,4 +26,13 @@ class Customer{
         std::string cognome,
         std::string mail,
         int città);
+
+        void AggiungiIndirizzo(
+            Con2DB db1,
+            std::string via,
+            int civico,
+            std::string cap,
+            std::string city,
+            std::string stato
+        );
 };
