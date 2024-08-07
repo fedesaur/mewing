217510ac75dc1677445982a6d11fda6800c70436
FedericaS <fedesa98@gmail.com>
2024-08-07 11:56:45 +0200

    prova server

---
+++ build/CMakeFiles/ecommerce_server.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ecommerce_server.dir/src/main.cpp.o"
+  "CMakeFiles/ecommerce_server.dir/src/main.cpp.o.d"
+  "ecommerce_server"
+  "ecommerce_server.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/ecommerce_server.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/CMakeFiles/ecommerce_server.dir/compiler_depend.make
@@ -0,0 +1,2 @@
+# Empty compiler generated dependencies file for ecommerce_server.
+# This may be replaced when dependencies are built.
