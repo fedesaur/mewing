1bad52a299263ecdcc62b13bc76ca59308fa74fd
simyKek <122544997+simyKek@users.noreply.github.com>
2024-08-13 11:10:47 +0200

    Aggiornato create.sh

--- database/create.sh
+++ database/create.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-sudo -u postgres psql postgres -f funzioni.sql -f tabelle.sql  -f trigger.sql  -f parametri.sql
+sudo -u postgres psql postgres -f parametri.sql -f table.sql -f trigger.sql -f function.sql -f fill.sql 
