e11d27d20d6fbefc2f9ef65cdb6d3728ae87dbf0
simyKek <122544997+simyKek@users.noreply.github.com>
2024-08-14 20:57:27 +0200

    Update create.sh

--- database/create.sh
+++ database/create.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-sudo -u postgres psql postgres -f parametri.sql -f table.sql -f trigger.sql -f permission.sql -f setup.sql -f setup_log.sql -f table_log.sql -f function.sql -f fill.sql 
+sudo -u postgres psql postgres -f parametri.sql -f table.sql -f trigger.sql -f permission.sql -f setup.sql -f setup_log.sql -f table_log.sql -f trigger_log.sql -f function.sql -f fill.sql 
