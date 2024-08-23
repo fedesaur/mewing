9e41f9457046219ae4f12cca93f5954b5bee74c5
simyKek <simy8000@gmail.com>
2024-08-23 18:58:48 +0200

    Aggiorna autenticazione.cpp

--- entities/Customer/methods/Autenticazione/autenticazione.cpp
+++ entities/Customer/methods/Autenticazione/autenticazione.cpp
@@ -4,11 +4,10 @@ bool autentica(int IDConnessione)
 {
 	redisContext *c2r; // c2r contiene le info sul contesto
 	redisReply *reply; // reply contiene le risposte da Redis
-	/*
-         Now read the stream for authentication confirmation
-         Start reading from the `entryID`
-    */
-    std::cout << "Eccoti l'ID: " << IDConnessione << std::endl;
+	c2r = redisConnect(REDIS_IP, REDIS_PORT); // Effettua la connessione a Redis
+
+
+	std::cout << "Eccoti l'ID: " << IDConnessione << std::endl;
     reply = RedisCommand(c2r, "XREVRANGE %s + - COUNT 1", CUSTOMER_STREAM);
     if (reply == nullptr || reply->type != REDIS_REPLY_ARRAY || reply->elements == 0)
 	{
@@ -18,8 +17,9 @@ bool autentica(int IDConnessione)
 
     redisReply* stream = reply -> element[0];
     redisReply* entryFields = stream -> element[1];
-    std::string fieldName = entryFields->element[IDConnessione*2]->str; // Chiave
-   	std::string received_email = entryFields->element[IDConnessione*2 + 1]->str; // Valore
+    size_t index = IDConnessione*2;
+    std::string fieldName = entryFields->element[index]->str; // Chiave
+   	std::string received_email = entryFields->element[index+ 1]->str; // Valore
 
 	if (received_email.empty()) std::cerr << "Errore: non è stata trovata nessuna email con la chiave specificata." << std::endl;
 	else std::cout << "Email letta dallo stream: " << received_email << std::endl;
