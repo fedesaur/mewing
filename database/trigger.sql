ccc3de76581a471968a05a57ea4377cba0ab8ac1
edobughi <168537499+edobughi@users.noreply.github.com>
2024-09-04 22:44:54 +0200

    Update trigger.sql

--- database/trigger.sql
+++ database/trigger.sql
@@ -21,6 +21,7 @@ create or replace function rim_prod() returns trigger as $rimuovi_prodotto$
 	BEGIN
 		delete from prodincart where prodotto=OLD.id;
 		delete from inwish where prodotto=OLD.id;
+		delete from prodmet where prodotto=OLD.id;
 	END
 	$rimuovi_prodotto$
 		language plpgsql;
