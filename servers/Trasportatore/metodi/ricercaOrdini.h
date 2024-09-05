9da939625d0b49dbf4d1e5dd66305472a2360ff3
Kektus8000 <simy8000@gmail.com>
2024-09-05 11:29:36 +0200

    Primo Update di ricercaOrdini
    Ancora non è conclusa la funzione

---
+++ servers/Trasportatore/metodi/ricercaOrdini.h
@@ -0,0 +1,39 @@
+#ifndef RICERCAPRODOTTI_H
+#define RICERCAPRODOTTI_H
+
+#include <string> // Importa std::string
+#include <utility> //Importa pair
+#include <iostream>
+#include <sstream>
+#include <algorithm>
+#include "../../../lib/con2db/pgsql.h"
+#include "../../../lib/con2redis/src/con2redis.h"
+#include "../../../entities/Ordine.h"
+#include "../../../lib/funzioniAusiliarie.h"
+#include <netinet/in.h>
+#include <sys/socket.h>
+#include <unistd.h>
+#include <cstring>
+#include <cassert>
+
+#define WRITE_STREAM "CourierW"
+#define READ_STREAM "CourierR"
+
+#define REDIS_IP "localhost"
+#define REDIS_PORT 6379
+
+#define SERVER_PORT 5002
+
+#define HOSTNAME "localhost"
+#define DB_PORT "5432"
+#define USERNAME "courier"
+#define PASSWORD "courier"
+#define DB_NAME "mewingdb"
+
+bool ricercaOrdini(int clientSocket);
+std::pair<int, Prodotto*> recuperaProdottiDisponibili(int userID, Con2DB db, PGresult *res, int clientSocket);
+bool aggiungiAlCarrello(Con2DB db, PGresult *res, int USER_ID, std::pair<int, Prodotto*> carrello, std::pair<int, Prodotto*> disponibili, int clientSocket);
+bool aggiungiCarrelloDB(int idProdotto, int userID, int quantita, Con2DB db, PGresult *res);
+
+
+#endif
