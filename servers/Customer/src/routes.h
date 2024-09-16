7873d9325145d000824138a297dd7fb8bf5ed13e
fedesaur <104363014+fedesaur@users.noreply.github.com>
2024-09-16 17:14:37 +0200

    Update routes.h

--- servers/Customer/src/routes.h
+++ servers/Customer/src/routes.h
@@ -1,3 +1,6 @@
+#ifndef ROUTES_H
+#define ROUTES_H
+
 #pragma once
 #include <pistache/router.h>
 
@@ -5,3 +8,5 @@ void defineRoutes(Pistache::Rest::Router& router);
 
 void authenticateUser(const Pistache::Rest::Request& request, Pistache::Http::ResponseWriter response);
 void modificaNomeHttp(const Pistache::Rest::Request& request, Pistache::Http::ResponseWriter response);
+
+#endif // ROUTES_H
