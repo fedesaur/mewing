9343c1fede9b46710f9592a6811ad22369f1746b
fedesaur <104363014+fedesaur@users.noreply.github.com>
2024-09-16 17:17:42 +0200

    Update routes.cpp

--- servers/Customer/src/routes.cpp
+++ servers/Customer/src/routes.cpp
@@ -6,8 +6,8 @@
 
 void defineRoutes(Pistache::Rest::Router& router) {
     // Registrazione delle rotte con funzioni globali
-    Pistache::Rest::Routes::Post(router, "/autentica", authenticateUser);
-    Pistache::Rest::Routes::Post(router, "/modificaNome", modificaNomeHttp);
+    Pistache::Rest::Routes::Post(router, "/autentica", Pistache::Rest::Routes::bind(&authenticateUser));
+    Pistache::Rest::Routes::Post(router, "/modificaNome", Pistache::Rest::Routes::bind(&modificaNomeHttp));
 }
 
 void authenticateUser(const Pistache::Rest::Request& request, Pistache::Http::ResponseWriter response) {
@@ -19,3 +19,4 @@ void modificaNomeHttp(const Pistache::Rest::Request& request, Pistache::Http::Re
     // Implementa la logica di modifica del nome qui
     response.send(Pistache::Http::Code::Ok, "Name updated");
 }
+
