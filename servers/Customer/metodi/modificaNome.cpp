9db8c80b4b7723b7519066c734019c4639cb97f8
fedesaur <104363014+fedesaur@users.noreply.github.com>
2024-09-16 12:05:45 +0200

    Update modificaNome.cpp

--- servers/Customer/metodi/modificaNome.cpp
+++ servers/Customer/metodi/modificaNome.cpp
@@ -1,6 +1,6 @@
 #include "modificaNome.h"
 
-bool modificaNomeHttp(const crow::request& req, crow::response& res) {
+bool modificaNomeHttp(const Pistache::Rest::Request& req, Pistache::Http::ResponseWriter response) {
     int USER_ID;
     PGresult *resDb;
     redisContext *c2r;
@@ -9,12 +9,16 @@ bool modificaNomeHttp(const crow::request& req, crow::response& res) {
     std::string nome, cognome;
     
     c2r = redisConnect(REDIS_IP, REDIS_PORT);
+    if (c2r == nullptr || c2r->err) {
+        response.send(Pistache::Http::Code::Internal_Server_Error, "Errore nella connessione a Redis");
+        return false;
+    }
+
     Con2DB db(HOSTNAME, DB_PORT, USERNAME_CUST, PASSWORD_CUST, DB_NAME);
 
     reply = RedisCommand(c2r, "XREVRANGE %s + - COUNT 1", READ_STREAM);
     if (reply == nullptr || reply->type != REDIS_REPLY_ARRAY || reply->elements == 0) {
-        res.code = 500;
-        res.body = "Errore nel comando Redis o stream vuoto";
+        response.send(Pistache::Http::Code::Internal_Server_Error, "Errore nel comando Redis o stream vuoto");
         return false;
     }
     std::string id = reply->element[0]->element[1]->element[1]->str;
@@ -23,8 +27,7 @@ bool modificaNomeHttp(const crow::request& req, crow::response& res) {
     sprintf(comando, "SELECT nome, cognome FROM customers WHERE id = %d", USER_ID);
     resDb = db.ExecSQLtuples(comando);
     if (PQresultStatus(resDb) != PGRES_TUPLES_OK) {
-        res.code = 500;
-        res.body = "Errore nel recupero dei dati dal database";
+        response.send(Pistache::Http::Code::Internal_Server_Error, "Errore nel recupero dei dati dal database");
         return false;
     }
 
@@ -32,38 +35,33 @@ bool modificaNomeHttp(const crow::request& req, crow::response& res) {
     cognome = PQgetvalue(resDb, 0, PQfnumber(resDb, "cognome"));
 
     // Supponiamo che il corpo della richiesta contenga l'azione e i nuovi dati
-    std::string action = req.url_params.get("action");
-    std::string newValue = req.body;
+    std::string action = req.query().get("action").get_or_else("");
+    std::string newValue = req.body();
 
     if (action == "cambia_nome") {
         if (newValue.length() > 20 || newValue.length() == 0) {
-            res.code = 400;
-            res.body = "Il nome deve avere massimo 20 caratteri e minimo 1";
+            response.send(Pistache::Http::Code::Bad_Request, "Il nome deve avere massimo 20 caratteri e minimo 1");
             return false;
         }
         sprintf(comando, "UPDATE customers SET nome = '%s' WHERE id = %d", newValue.c_str(), USER_ID);
     } else if (action == "cambia_cognome") {
         if (newValue.length() > 20 || newValue.length() == 0) {
-            res.code = 400;
-            res.body = "Il cognome deve avere massimo 20 caratteri e minimo 1";
+            response.send(Pistache::Http::Code::Bad_Request, "Il cognome deve avere massimo 20 caratteri e minimo 1");
             return false;
         }
         sprintf(comando, "UPDATE customers SET cognome = '%s' WHERE id = %d", newValue.c_str(), USER_ID);
     } else {
-        res.code = 400;
-        res.body = "Azione non valida";
+        response.send(Pistache::Http::Code::Bad_Request, "Azione non valida");
         return false;
     }
 
     resDb = db.ExecSQLcmd(comando);
     if (PQresultStatus(resDb) != PGRES_COMMAND_OK) {
-        res.code = 500;
-        res.body = "Errore nell'aggiornamento del database";
+        response.send(Pistache::Http::Code::Internal_Server_Error, "Errore nell'aggiornamento del database");
         return false;
     }
 
-    res.code = 200;
-    res.body = "Operazione completata con successo";
+    response.send(Pistache::Http::Code::Ok, "Operazione completata con successo");
     PQclear(resDb);
     return true;
 }
