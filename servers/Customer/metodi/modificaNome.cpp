9e26268050497d1f9e5aaffd26d06f49855d2244
fedesaur <104363014+fedesaur@users.noreply.github.com>
2024-09-16 11:33:50 +0200

    Update modificaNome.cpp

--- servers/Customer/metodi/modificaNome.cpp
+++ servers/Customer/metodi/modificaNome.cpp
@@ -1,153 +1,69 @@
 #include "modificaNome.h"
 
-bool modificaNome(int clientSocket)
-{
+bool modificaNomeHttp(const crow::request& req, crow::response& res) {
     int USER_ID;
-    PGresult *res;
-    redisContext *c2r; // c2r contiene le info sul contesto
-	redisReply *reply; // reply contiene le risposte da Redis
+    PGresult *resDb;
+    redisContext *c2r;
+    redisReply *reply;
     char comando[1000];
-    char buffer[1024] = {0};
-    int NUMERO_OPZIONI = 3;
-    std::string OPZIONI[] = {"1) Cambia Nome\n", "2) Cambia Cognome\n", "Altrimenti digita Q per terminare\n"};
-
-	c2r = redisConnect(REDIS_IP, REDIS_PORT); // Effettua la connessione a Redis
-	Con2DB db(HOSTNAME, DB_PORT, USERNAME_CUST, PASSWORD_CUST, DB_NAME); // Effettua la connessione al database
+    std::string nome, cognome;
+    
+    c2r = redisConnect(REDIS_IP, REDIS_PORT);
+    Con2DB db(HOSTNAME, DB_PORT, USERNAME_CUST, PASSWORD_CUST, DB_NAME);
 
     reply = RedisCommand(c2r, "XREVRANGE %s + - COUNT 1", READ_STREAM);
-    if (reply == nullptr || reply->type != REDIS_REPLY_ARRAY || reply->elements == 0)
-    {
-        std::cerr << "Errore nel comando Redis o stream vuoto" << std::endl;
+    if (reply == nullptr || reply->type != REDIS_REPLY_ARRAY || reply->elements == 0) {
+        res.code = 500;
+        res.body = "Errore nel comando Redis o stream vuoto";
         return false;
     }
     std::string id = reply->element[0]->element[1]->element[1]->str;
-    USER_ID = stoi(id);
-    
+    USER_ID = std::stoi(id);
+
     sprintf(comando, "SELECT nome, cognome FROM customers WHERE id = %d", USER_ID);
-    res = db.ExecSQLtuples(comando);
-    if (PQresultStatus(res) != PGRES_TUPLES_OK) return false;
-    // Recupera nome e cognome dell'utente dal database
-    std::string nome = PQgetvalue(res, 0, PQfnumber(res, "nome"));
-    std::string cognome = PQgetvalue(res, 0, PQfnumber(res, "cognome"));
-    
-    bool concludiOperazione = false;
-    do
-    {
-        // Mostra nome e cognome all'utente
-        std::string request = "\nNome attuale: " + nome + " Cognome Attuale: " + cognome + "\n"; 
-	    send(clientSocket, request.c_str(), request.length(), 0);
-        //Stampa all'utente le opzioni disponibili
-        for (int i = 0; i < NUMERO_OPZIONI; i++) send(clientSocket, OPZIONI[i].c_str(), OPZIONI[i].length(), 0);
-        int bytesRead = recv(clientSocket, buffer, sizeof(buffer) - 1, 0);
-        if (bytesRead > 0)
-        {
-            std::string messaggio(buffer, bytesRead);
-            messaggio.erase(std::remove(messaggio.begin(), messaggio.end(), '\n'), messaggio.end()); // Rimuove eventuali newline
-
-            if (messaggio == "q" || messaggio == "Q") {
-                concludiOperazione = true; // Termina la connessione
-            } else if (std::isdigit(messaggio[0])) {
-                int opzione = std::stoi(messaggio) - 1;
-                std::pair<std::string,bool> risultato;
-                switch (opzione)
-                {
-                    case 0:
-                        risultato = cambiaNome(db, res, clientSocket, USER_ID);
-                        if (risultato.second)
-                        {
-                            std::string conferma = "Nome cambiato!\n";
-                            send(clientSocket, conferma.c_str(), conferma.length(), 0);
-                            nome = risultato.first;
-                        }
-                        break;
-                    case 1:
-                        risultato = cambiaCognome(db, res, clientSocket, USER_ID);
-                        if (risultato.second)
-                        {   
-                            std::string conferma = "Cognome cambiato!\n";
-                            send(clientSocket, conferma.c_str(), conferma.length(), 0);
-                            cognome = risultato.first;
-                        }        
-                        break;
-                    default: // Se gli altri casi non sono stati accettati...
-                        std::string errore = "Opzione non valida, riprova.\n";
-                        send(clientSocket, errore.c_str(), errore.length(), 0);
-                        break;
-                }
-            } else {
-            std::string errore = "Input non valido, riprova.\n";
-            send(clientSocket, errore.c_str(), errore.length(), 0);
-            }
-        }
-    } while (!concludiOperazione);
-    PQclear(res);
-    return true;   
-}
-
-std::pair<std::string,bool> cambiaNome(Con2DB db, PGresult *res, int clientSocket, int USER_ID)
-{
-    char comando[1000];
-    char buffer[1024] = {0};
-    std::pair<std::string,bool> risultato;
-    std::string request = "Inserisci il nuovo nome\n"; 
-	int bytesRead = recv(clientSocket, buffer, sizeof(buffer) - 1, 0);
-    if (bytesRead > 0)
-    {
-        // Chiede all'utente il nuovo nome
-        std::string messaggio(buffer, bytesRead);
-        messaggio.erase(std::remove(messaggio.begin(), messaggio.end(), '\n'), messaggio.end()); // Rimuove eventuali newline
-        if (messaggio.length() > 20 || messaggio.length() == 0)
-        {                    
-            std::string errore = "Il nome deve avere massimo 20 caratteri e minimo 0\n";
-            send(clientSocket, errore.c_str(), errore.length(), 0);
-            risultato.first = "";
-            risultato.second = false;
-            return risultato;
-        }
-        sprintf(comando, "UPDATE customers SET nome = '%s' WHERE id = %d", messaggio.c_str(), USER_ID);
-        res = db.ExecSQLcmd(comando);
-        risultato.first = messaggio;
-        risultato.second = true;
-        PQclear(res);
-        return risultato;
+    resDb = db.ExecSQLtuples(comando);
+    if (PQresultStatus(resDb) != PGRES_TUPLES_OK) {
+        res.code = 500;
+        res.body = "Errore nel recupero dei dati dal database";
+        return false;
     }
-    std::string errore = "Input non valido\n";
-    send(clientSocket, errore.c_str(), errore.length(), 0);
-    risultato.first = "";
-    risultato.second = false;
-    return risultato;
-}
 
-std::pair<std::string,bool> cambiaCognome(Con2DB db, PGresult *res, int clientSocket, int USER_ID)
-{
-    char comando[1000];
-    char buffer[1024] = {0};
-    std::pair<std::string,bool> risultato;
-    std::string request = "Inserisci il nuovo cognome\n";
-	int bytesRead = recv(clientSocket, buffer, sizeof(buffer) - 1, 0);
-    if (bytesRead > 0)
-    {
-        // Chiede all'utente il nuovo nome
-        std::string messaggio(buffer, bytesRead);
-        messaggio.erase(std::remove(messaggio.begin(), messaggio.end(), '\n'), messaggio.end()); // Rimuove eventuali newline
-        if (messaggio.length() > 20 || messaggio.length() == 0)
-        {                    
-            std::string errore = "Il cognome deve avere massimo 20 caratteri e minimo 0\n";
-            send(clientSocket, errore.c_str(), errore.length(), 0);
-            risultato.first = "";
-            risultato.second = false;
-            return risultato;
+    nome = PQgetvalue(resDb, 0, PQfnumber(resDb, "nome"));
+    cognome = PQgetvalue(resDb, 0, PQfnumber(resDb, "cognome"));
+
+    // Supponiamo che il corpo della richiesta contenga l'azione e i nuovi dati
+    std::string action = req.url_params.get("action");
+    std::string newValue = req.body;
+
+    if (action == "cambia_nome") {
+        if (newValue.length() > 20 || newValue.length() == 0) {
+            res.code = 400;
+            res.body = "Il nome deve avere massimo 20 caratteri e minimo 1";
+            return false;
         }
-        sprintf(comando, "UPDATE customers SET cognome = '%s' WHERE id = %d", messaggio.c_str(), USER_ID);
-        res = db.ExecSQLcmd(comando);
-        risultato.first = messaggio;
-        risultato.second = true;
-        PQclear(res);
-        return risultato;
+        sprintf(comando, "UPDATE customers SET nome = '%s' WHERE id = %d", newValue.c_str(), USER_ID);
+    } else if (action == "cambia_cognome") {
+        if (newValue.length() > 20 || newValue.length() == 0) {
+            res.code = 400;
+            res.body = "Il cognome deve avere massimo 20 caratteri e minimo 1";
+            return false;
+        }
+        sprintf(comando, "UPDATE customers SET cognome = '%s' WHERE id = %d", newValue.c_str(), USER_ID);
+    } else {
+        res.code = 400;
+        res.body = "Azione non valida";
+        return false;
     }
-    std::string errore = "Input non valido\n";
-    send(clientSocket, errore.c_str(), errore.length(), 0);
-    risultato.first = "";
-    risultato.second = false;
-    return risultato;
+
+    resDb = db.ExecSQLcmd(comando);
+    if (PQresultStatus(resDb) != PGRES_COMMAND_OK) {
+        res.code = 500;
+        res.body = "Errore nell'aggiornamento del database";
+        return false;
+    }
+
+    res.code = 200;
+    res.body = "Operazione completata con successo";
+    PQclear(resDb);
+    return true;
 }
