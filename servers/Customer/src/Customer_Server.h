d851bc5ce6c266930fb45bc1c49c4574292cee37
fedesaur <104363014+fedesaur@users.noreply.github.com>
2024-09-16 11:58:09 +0200

    Update Customer_Server.h

--- servers/Customer/src/Customer_Server.h
+++ servers/Customer/src/Customer_Server.h
@@ -11,49 +11,43 @@
 #include "../metodi/modificaNome.h"
 #include "../metodi/gestioneIndirizzi.h"
 #include "../metodi/gestisciMetodi.h"
-#include <netinet/in.h>
-#include <sys/socket.h>
-#include <unistd.h>
-#include <cstring>
-#include <cassert>
 #include <thread>
 #include <mutex>
-
-#include "crow.h"
-#include "routes.h"
+#include <pistache/endpoint.h>
+#include <pistache/router.h>
 
 #define WRITE_STREAM "CustomerW"
 #define READ_STREAM "CustomerR"
 #define REDIS_IP "localhost"
 #define REDIS_PORT 6379
 #define SERVER_PORT 5000
-#define MAX_CONNECTIONS 100 //Numero di connessioni massime accettabili
+#define MAX_CONNECTIONS 100 // Numero di connessioni massime accettabili
 
 class Customer_Server
 {
-    private:
-        // I parametri del Customer teniamoli privati per sicurezza
-        redisContext *c2r; // c2r contiene le info sul contesto
-        redisReply *reply; // reply contiene le risposte da Redis
-        int ID_CONNESSIONE = 0;
-        int SERVER_SOCKET;
-        int NUMERO_OPZIONI = 5;
-        std::string OPZIONI[5]; // Opzioni dell'utente
-        std::mutex id_mutex;
-        crow::SimpleApp app;
-        std::thread crowThread; // Thread per il server Crow
+private:
+    // I parametri del Customer teniamoli privati per sicurezza
+    redisContext *c2r; // c2r contiene le info sul contesto
+    redisReply *reply; // reply contiene le risposte da Redis
+    int ID_CONNESSIONE = 0;
+    int NUMERO_OPZIONI = 5;
+    std::string OPZIONI[5]; // Opzioni dell'utente
+    std::mutex id_mutex;
+    
+    // Funzioni per la gestione delle richieste HTTP con Pistache
+    void defineRoutes();
+    static void handleOptions(const Pistache::Rest::Request& request, Pistache::Http::ResponseWriter response);
 
-        // Funzione per eseguire Crow in un thread separato
-        void startCrow();
-        bool handshake(int clientSocket);
-        void gestisciConnessioneCliente(int clientSocket, int connectionID);
-        bool gestisciAutenticazione(int clientSocket);
-        bool gestisciOperazioni(int clientSocket);
-    public:
-        Customer_Server(); // Costruttore di Customer
-        void gestisciConnessioni(); // Metodi di Customer
+    // Funzioni per la connessione a Redis e gestione dei clienti
+    bool handshake(int clientSocket);
+    void gestisciConnessioneCliente(int clientSocket, int connectionID);
+    bool gestisciAutenticazione(int clientSocket);
+    bool gestisciOperazioni(int clientSocket);
+
+public:
+    Customer_Server(); // Costruttore di Customer
+    ~Customer_Server(); // Distruttore di Customer
+    void startPistache(); // Avvia il server Pistache
 };
 
-
-
-#endif //CUSTOMER_SERVER_H
+#endif // CUSTOMER_SERVER_H
