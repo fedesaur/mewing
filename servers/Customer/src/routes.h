ebfb15136d0705425ed465af0004eb37d24b0005
fedesaur <104363014+fedesaur@users.noreply.github.com>
2024-09-17 17:32:42 +0200

    Update routes.h

--- servers/Customer/src/routes.h
+++ servers/Customer/src/routes.h
@@ -5,11 +5,14 @@
 #include <pistache/router.h>
 #include "../../../lib/con2db/pgsql.h"
 #include "../../../lib/con2redis/src/con2redis.h"
+#include "../metodi/aggiungiCarrello.h"
 
 void defineRoutes(Pistache::Rest::Router& router);
 
 void authenticateUser(const Pistache::Rest::Request& request, Pistache::Http::ResponseWriter response);
 void modificaNomeHttp(const Pistache::Rest::Request& request, Pistache::Http::ResponseWriter response);
 void getProdotti(const Pistache::Rest::Request& request, Pistache::Http::ResponseWriter response);
+void addProdottoToCarrello(const Pistache::Rest::Request& request, Pistache::Http::ResponseWriter response);
+int recuperaCustomerID(const std::string& email);
 
 #endif // ROUTES_H
