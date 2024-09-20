d3eb3bb74b8c8b9eb5c3bf8937c3fba1e366cda5
simyKek <122544997+simyKek@users.noreply.github.com>
2024-09-20 15:41:35 +0200

    Fix vari

--- database/permission.sql
+++ database/permission.sql
@@ -54,7 +54,7 @@ GRANT INSERT, UPDATE, SELECT, DELETE on custadd to :customer;
 GRANT INSERT, UPDATE, SELECT, DELETE on indirizzo to :customer;
 GRANT INSERT, UPDATE, SELECT, DELETE on inwish to :customer;
 GRANT INSERT, UPDATE, SELECT, DELETE on metpag to :customer;
-GRANT INSERT, UPDATE, DELETE on ordine to :customer;
+GRANT SELECT, INSERT, UPDATE, DELETE on ordine to :customer;
 GRANT INSERT, UPDATE, SELECT, DELETE on prodincart to :customer;
 GRANT SELECT on prodotto to :customer;
 GRANT SELECT on fornitore to :customer;
--- install.sh
+++ install.sh
@@ -5,5 +5,7 @@ sudo apt-get install postgresql
 sudo apt-get install redis
 sudo apt-get install libhiredis-dev
 sudo apt install libpq-dev
+sudo apt install libpistache-dev
+sudo apt install curl
 
 sudo usermod -aG $USER postgres
