96f91a97677db903343404d7eceebc2da2568116
edobughi <168537499+edobughi@users.noreply.github.com>
2024-10-02 21:21:15 +0200

    Update trigger_log.sql

--- database/trigger_log.sql
+++ database/trigger_log.sql
@@ -13,16 +13,14 @@ BEGIN
         EXECUTE 'DROP FUNCTION IF EXISTS ' || function_name || ' CASCADE';
     END LOOP;
 END $$;
-
-
-CREATE OR REPLACE FUNCTION consistent_file_descriptor() RETURNS TRIGGER AS $$
+-------------------------------------------------------------------------
+CREATE OR REPLACE FUNCTION consistent_Operation() RETURNS TRIGGER AS $$
 BEGIN
 	IF EXISTS(
 			SELECT 1
 				FROM cliente
-				WHERE NEW.nome_server=client.nome_server
-				AND NEW.file_descriptor=cliente.file_descriptor
-				AND (cliente.tempo_disconnessione IS NULL OR NEW.tempo_connessione <= cliente.tempo_disconnessione)
+				WHERE NEW.User_Id=client.User_Id
+				AND (cliente.Data_termine IS NULL OR NEW.Data_inizio <= cliente.Data_termine)
 		)	IS TRUE THEN
 				RAISE EXCEPTION 'connessione file descriptor non chiusa';
 	END IF;
@@ -32,5 +30,5 @@ END;
 
 $$ LANGUAGE plpgsql;
 
-CREATE TRIGGER consistent_file_descriptor_trg
-	BEFORE INSERT ON cliente FOR EACH ROW EXECUTE FUNCTION consistent_file_descriptor();
+CREATE TRIGGER consistent_Operation_trg
+	BEFORE INSERT ON cliente FOR EACH ROW EXECUTE FUNCTION consistent_Operation();
