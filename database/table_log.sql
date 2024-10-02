3b7bcf30e3202310860501e5d345b5962199da24
edobughi <168537499+edobughi@users.noreply.github.com>
2024-10-02 22:31:40 +0200

    Update table_log.sql

--- database/table_log.sql
+++ database/table_log.sql
@@ -3,14 +3,16 @@ CREATE type TipoUtenteType as ENUM ('customer','produttore','trasportatore');
 CREATE type EsitoType as ENUM ('Successo','Fallito');
 CREATE sequence Cliente_id_sequence;
 
-CREATE TABLE cliente(User_Id integer not null DEFAULT nextval('Cliente_id_sequence'::regclass),
+CREATE TABLE cliente(Id integer not null DEFAULT nextval('Cliente_id_sequence'::regclass),
+		     Cliente_id Integer not null
 		     TipoUser TipoUtenteType not null,
 		     OperationTipe varchar(50) not null,
 		     Data_inizio timestamp not null,
 	   	     Data_termine timestamp,
 	 	     Esito EsitoType,
 		     
-			PRIMARY KEY (User_Id,TipoUser,Data_inizio),
+			PRIMARY KEY (Id),
+			UNIQUE(User_Id,TipoUser,Data_inizio),
 			CONSTRAINT check_tempo_inizio_fine check(Data_termine IS NULL OR Data_termine>=Data_inizio)
 			);
 
