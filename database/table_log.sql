d0d2c2b943e7912c0765dfe7e251510a95a4ddf6
edobughi <168537499+edobughi@users.noreply.github.com>
2024-10-02 21:16:15 +0200

    Update table_log.sql

--- database/table_log.sql
+++ database/table_log.sql
@@ -1,37 +1,16 @@
 \c :log_dbname
-CREATE DOMAIN Pos as integer check (value>0);
-CREATE TYPE TipoStatisticheSessione AS ENUM ('Sessione', 'Risposta');
+CREATE type TipoUtenteType as ENUM ('customer','produttore','trasportatore');
+CREATE type EsitoType as ENUM ('Successo','Fallito');
 
-CREATE TABLE cliente(
-					nome_server VARCHAR(50) NOT NULL,
-					file_descriptor integer NOT NULL,
-					tempo_connessione timestamp NOT NULL,
-					tempo_disconnessione timestamp,
-					PRIMARY KEY(nome_server, file_descriptor,tempo_connessione),
-					CONSTRAINT check_tempo_disconnessione CHECK(tempo_disconnessione IS NULL OR tempo_disconnessione>= tempo_connessione)
-					);
-
-CREATE TABLE comunicazione(
-			richiesta VARCHAR(1000) NOT NULL,
-			tempo_richiesta timestamp NOT NULL,
-			stato_risposta VARCHAR(50),
-			tempo_risposta timestamp ,
-			nome_server_cliente VARCHAR(50) NOT NULL,
-			descrizione_file_cliente integer NOT NULL,
-			tempo_connessione_cliente timestamp NOT NULL,
-
-			PRIMARY KEY(nome_server_cliente, descrizione_file_cliente, tempo_connessione_cliente, tempo_richiesta),
-			FOREIGN KEY(nome_server_cliente, descrizione_file_cliente, tempo_connessione_cliente) REFERENCES cliente (nome_server, file_descriptor, tempo_connessione),
-			CONSTRAINT risposta_dopo_richiesta CHECK (tempo_risposta IS NULL OR tempo_risposta>=tempo_richiesta),
-			CONSTRAINT richiesta_dopo_connessione CHECK(tempo_richiesta>=tempo_connessione_cliente)
+CREATE TABLE cliente(User_Id integer not null,
+		     TipoUser TipoUtenteType not null,
+		     OperationTipe varchar(50) not null,
+		     Data_inizio timestamp not null,
+	   	     Data_ternime timestamp,
+	 	     Esito EsitoType,
+		     
+			PRIMARY KEY (User_Id,TipoUser,Data_inizio),
+			CONSTRAINT check_tempo_inizio_fine check(Data_termine IS NULL OR Data_termine>=Data_inizio)
 			);
 
-CREATE TABLE statisticheSessione(
-			id SERIAL NOT NULL PRIMARY KEY,
-			tipo TipoStatisticheSessione NOT NULL,
-			istante_finale timestamp NOT NULL,
-			valore Pos NOT NULL,
-			stato_risposta VARCHAR(50) NOT NULL,
 
-			UNIQUE (tipo, istante_finale)
-			);
